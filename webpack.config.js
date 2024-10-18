5135b3d5c099b037e4eed9415ac50f95cf3a3da1
Nick-Major <divanchik26@gmail.com>
2024-10-18 15:38:11 +0300

    Added Jest

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.txt$/,
+                loader: 'raw-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
